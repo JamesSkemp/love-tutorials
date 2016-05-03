2fe82665cf87c1f947f7dbe75186de552c1db334
James Skemp <strivinglife@gmail.com>
2016-05-02 20:47:10 -0500

    Add functionality to display actual content for second hello world example.

--- packt/hello-world-2/main.lua
+++ packt/hello-world-2/main.lua
@@ -2,7 +2,14 @@
 
 -- Load assets.
 function love.load()
-
+	-- Add a new font with size 45.
+	local myfont = love.graphics.newFont(45)
+	-- Load the font we defined above.
+	love.graphics.setFont(myfont)
+	-- rgba
+	love.graphics.setColor(0, 0, 0, 225)
+	-- rgb
+	love.graphics.setBackgroundColor(255, 153, 0)
 end
 
 -- Update, with dt time since last update.
@@ -12,5 +19,5 @@ end
 
 -- Draw to the display.
 function love.draw()
-
+	love.graphics.print('Hello World!', 200, 200)
 end
