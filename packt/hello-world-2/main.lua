128e48c855618520c446e7f245c8d25a01b12f63
James Skemp <strivinglife@gmail.com>
2016-05-02 20:43:00 -0500

    Add basic game structure to new hello world example.

---
+++ packt/hello-world-2/main.lua
@@ -0,0 +1,16 @@
+-- Basic LOVE game structure:
+
+-- Load assets.
+function love.load()
+
+end
+
+-- Update, with dt time since last update.
+function love.update(dt)
+
+end
+
+-- Draw to the display.
+function love.draw()
+
+end
