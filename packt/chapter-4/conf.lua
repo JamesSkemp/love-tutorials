2ba92f8856ba377a51bba5d05ee7ef118d8a3a59
James Skemp <strivinglife@gmail.com>
2016-05-06 19:48:25 -0500

    Add background and set basic window information.

---
+++ packt/chapter-4/conf.lua
@@ -0,0 +1,7 @@
+-- For all settings see https://love2d.org/wiki/Config_Files
+function love.conf(t)
+	t.window.width = 640
+	t.window.height = 480
+	t.window.title = 'Untitled'
+
+end
--- packt/chapter-4/main.lua
+++ packt/chapter-4/main.lua
@@ -1,5 +1,6 @@
 -- This function gets called only once, when the game is started, and is usually where you would load resources, initialize variables and set specific settings. All those things can be done anywhere else as well, but doing them here means that they are done once only, saving a lot of system resources.
 function love.load()
+	bg = love.graphics.newImage('area02_bkg0.jpg')
 
 end
 
@@ -10,5 +11,7 @@ end
 
 -- love.draw is where all the drawing happens (if that wasn't obvious enough already) and if you call any of the love.graphics.draw outside of this function then it's not going to have any effect. This function is also called continuously so keep in mind that if you change the font/color/mode/etc at the end of the function then it will have a effect on things at the beginning of the function. For example:
 function love.draw()
+	love.graphics.draw(bg)
+
 
 end
